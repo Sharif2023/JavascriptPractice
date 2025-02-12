1755ab8530cb4ba0fba108ef7bb3a1b97c4e3c19
Sharif2023 <sharifislam0505@gmail.com>
2025-02-12 16:18:41 +0600

    Professional way to accept input

--- project1/index.js
+++ project1/index.js
@@ -2,8 +2,15 @@
 
 //1. Easy Way
 
-let username = window.prompt("What's your user name");
-console.log(username); //username input will be shown there
+// let username = window.prompt("What's your user name");
+// console.log(username); //username input will be shown there
 
 //2. Professional Way using html
 
+let username;
+
+document.getElementById("submit").onclick = function(){
+    username = document.getElementById("username").value;
+    console.log(username);
+    document.getElementById("myh1").textContent = `Hello ${username}`; //added next to h1 by adding id to h1 to change welcome text
+}
