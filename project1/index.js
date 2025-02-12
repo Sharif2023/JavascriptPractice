42661717d1c0fe2c035181dbd08c848326a4f9eb
Sharif2023 <sharifislam0505@gmail.com>
2025-02-12 11:39:33 +0600

    Variable Declare and assign

--- project1/index.js
+++ project1/index.js
@@ -1,1 +1,15 @@
-document.getElementById("myH1").textContent = "Hello";
+//variable declaration & assignment
+
+let name = "Sharif", price = 10.99, age= 10, isStudent= true;
+console.log(`My name is ${name} and my age is ${age} and I bought a pen of ${price} tk `);
+
+console.log(typeof age); //output: number
+console.log(typeof name); //output: string
+
+let online= true;
+console.log(typeof online); //output: boolean
+
+//p1 output showing
+document.getElementById("myName").textContent = `My name is ${name}`;
+document.getElementById("age").textContent = `My age is ${age}`;
+document.getElementById("isStudent").textContent = `Student Status: ${isStudent}`;
