4652d278d6af723895792345a5f0308b3c61a8e4
Sharif2023 <sharifislam0505@gmail.com>
2025-02-13 10:51:49 +0600

    Math lib

--- project1/index.js
+++ project1/index.js
@@ -1,14 +1,29 @@
-//const = a variable that can't be changed
+//Math = build-in objects provides collection of build in properties and methods
+console.log(Math.PI);
+console.log(Math.E);
 
-const PI = 3.1416;
-let radius;
-radius = Number(radius);
-let circumferance;
+let x=3.21, y= 2, z, n=-2.2;
 
-//PI = 420.15; not work because can't reassign const
+// z = Math.log(y);
+// z = Math.sin(x);
+// z = Math.cos(y);
 
-document.getElementById("submit").onclick = function(){
-    radius = document.getElementById("radius").value
-    circumferance = 2*PI*radius;
-    document.getElementById("myh3").textContent = circumferance;
-}
+// z=Math.abs(n); //absolute will be positive
+// console.log(z);
+
+//sign(variable) will show 0 (positive) and 1 negative
+
+// let max = Math.max(x,y,n);
+// console.log(max);
+let min = Math.min(x, y, n);
+console.log(min);
+
+// console.log(Math.round(x));//int
+// console.log(Math.floor(x));//int
+// console.log(Math.ceil(x));//int but for float numbers it will convert it to next value
+// console.log(Math.trunc(x));//cut the float value
+// console.log(Math.pow(x,y));
+// console.log(Math.sqrt(x));
+
+// z = Math.log(y);
+// console.log(z);
