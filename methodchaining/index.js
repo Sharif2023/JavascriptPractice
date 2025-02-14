ca213f8c4f96b03aba9d24a4261c05fda88773f1
Sharif2023 <sharifislam0505@gmail.com>
2025-02-14 23:41:47 +0600

    Operator

--- methodchaining/index.js
+++ methodchaining/index.js
@@ -1,20 +1,5 @@
-//method chaining = calling one method after another
-                    //in one continuous line of code
-
-//-------No method chaining---------
-
-// let username = window.prompt("Enter your username: ");
-
-// username = username.trim(); //space remove
-// let letter = username.charAt(0);
-// letter = letter.toUpperCase(); //Make first character Big
-
-// let extrachars = username.slice(1); //index 1 to other
-// extrachars = extrachars.toLocaleLowerCase(); //make extras to small char
-// username = letter + extrachars; //merge 1st chart + extra chars
-
-// console.log(username);
-
-//------Method Chaining--------
-username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
-console.log(username);
+//     = assignment operator
+//    == comparison oprator (compare if values are equal)
+//   === strict equality operator (compare if values & datatype are equal)
+//    != inequality operator
+//   !== strict inequality operator
