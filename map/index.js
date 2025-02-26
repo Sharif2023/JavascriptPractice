e17bb4406a23f0de8323891eafd460b72d9bbb1e
Sharif2023 <sharifislam0505@gmail.com>
2025-02-26 19:41:28 +0600

    Arrow Function

--- map/index.js
+++ map/index.js
@@ -6,7 +6,52 @@ const formattedDates = dates.map(formateDates);
 
 console.log(formattedDates);
 
-function formateDates(elements){
+function formateDates(elements) {
     const parts = elements.split("-");
     return `${parts[1]}/${parts[2]}/${parts[0]}`;
 }
+
+
+///* Arrow Function *///
+
+//arrow function without parameter
+
+const hello = () => console.log("Hello!!!");
+hello();
+
+//arrow function with parameter
+
+const myName = (name) => console.log(`My name is ${name}`);
+myName("Sharif");
+
+//more console log with {}
+
+const myAge = (age) => {
+    console.log(`My age is ${age}`)
+    console.log(`Next year I'll 28`)
+};
+myAge(27);
+
+//multiple parameter
+
+const myNameAge = (myname, myage) => {
+    console.log(`My name is ${myname}`)
+    console.log(`My age is ${myage}`)
+};
+myNameAge("Sharif", 27);
+
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cube = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element%2 ===0);
+const oddNums = numbers.filter((element) => element%2 !==0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
+
+console.log(squares);
+console.log(cube);
+console.log(evenNums);
+console.log(oddNums);
+console.log(total);
