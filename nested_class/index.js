baa455f2e42f9a8ba15584d7f62fc068399fd658
Sharif2023 <sharifislam0505@gmail.com>
2025-03-06 21:03:29 +0600

    nested class

---
+++ nested_class/index.js
@@ -0,0 +1,15 @@
+const person = {
+    name: "spongebob",
+    age: 30,
+    hobby : ["fishing", "travelling", "gardening"],
+    address : {
+        city: "Dhaka",
+        postcode: 1214,
+    }
+}
+
+console.log(person.name);
+console.log(person.hobby);
+console.log(person.hobby[1]);
+console.log(person.address);
+console.log(person.address.city);
